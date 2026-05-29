3c3577a9ab5371fb9a62d8ab099a1f274e2c0120
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-29 11:46:39 +0100

    if else as an expression

--- main.zig
+++ main.zig
@@ -13,4 +13,15 @@ pub fn main() void{
     
     std.debug.print("LEN c: {d}\n",.{c.len});
     std.debug.print("LEN d: {d}\n",.{d.len});
+    
+    var  eval_flag = true;
+    var if_true_inc:i32 = 0; 
+    
+    if_true_inc += if (eval_flag) 1 else 0;
+    if_true_inc += if (eval_flag) 1 else 0;
+    if_true_inc += if (eval_flag) 1 else 0;
+    eval_flag = false;
+    if_true_inc += if (eval_flag) 1 else 0;
+    if_true_inc += if (eval_flag) 1 else 0;
+    std.debug.print("VAL if_true_inc: {d}\n",.{if_true_inc});
 }
