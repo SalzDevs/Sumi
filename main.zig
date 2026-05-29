712ebf55482da0d87197c53e8c4892e0d4d4de23
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-29 17:39:33 +0100

    error setssss

--- main.zig
+++ main.zig
@@ -1,24 +1,18 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
-fn addFive(x: u32) u32{
-    return x+5;
-}
+const FileOpenError = error{
+    AccessDenied,
+    OutOfMemory,
+    FileNotFound,
+};
 
-fn fibonacci(n: u16) u16{
-    if (n==0 or n==1) return n;
-    return fibonacci(n-1) + fibonacci(n-2);
-}
-
-fn addXtoY(x:i16,y:i16) i16{
-    return x+y;
+test "coerce error from a subset to a superset"{
+    const AllocationError = error{OutOfMemory};
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err==FileOpenError.OutOfMemory);
 }
 
 pub fn main() void{
-    var x:i16 = 16;
-    const y:i16 = 4;
-    if  (true){
-        defer x = addXtoY(x,y);
-        std.debug.print("x before defer:{d}\n",.{x});
-    }
-    std.debug.print("x after defer:{d}\n",.{x});
+ std.print.debug("Hello {s}\n",.{"Seamen"});
 }
