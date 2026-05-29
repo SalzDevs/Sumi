e5a540ab0571aee8570ff8abf74513f1e33da388
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-29 17:28:52 +0100

    defer baby

--- main.zig
+++ main.zig
@@ -9,47 +9,16 @@ fn fibonacci(n: u16) u16{
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
+fn addXtoY(x:i16,y:i16) i16{
+    return x+y;
+}
+
 pub fn main() void{
-    std.debug.print("Hello, {s}\n",.{"World"});
-    
-    const a: i32 = 5;
-    const b:i32 = 5000;
-
-    std.debug.print("VAL: {d}\n",.{a+b});
-
-    const c = [5]i32{1,2,3,4,5};
-    const d = [_]i32{};
-    
-    std.debug.print("LEN c: {d}\n",.{c.len});
-    std.debug.print("LEN d: {d}\n",.{d.len});
-    
-    var  eval_flag = true;
-    var if_true_inc:i32 = 0; 
-    
-    if_true_inc += if (eval_flag) 1 else 0;
-    if_true_inc += if (eval_flag) 1 else 0;
-    if_true_inc += if (eval_flag) 1 else 0;
-    eval_flag = false;
-    if_true_inc += if (eval_flag) 1 else 0;
-    if_true_inc += if (eval_flag) 1 else 0;
-    std.debug.print("VAL if_true_inc: {d}\n",.{if_true_inc});
-
-    var i:u32 = 0;
-    var sum = i;
-    
-    while(i <= 100) : (i+=3){
-        sum += if (i%2==0) i else continue;
-        std.debug.print("VAL sum:{d}\n",.{sum}); 
+    var x:i16 = 16;
+    const y:i16 = 4;
+    if  (true){
+        defer x = addXtoY(x,y);
+        std.debug.print("x before defer:{d}\n",.{x});
     }
-
-    const string = [_]u8{'h','l','l','f'};
-    
-    for (string,0..) |char,index|{
-        std.debug.print("VAL i:{d}\n",.{index}); 
-        std.debug.print("VAL character:{c}\n",.{char}); 
-    }
-    
-    const y = 10;
-    std.debug.print("VAL add_five(y):{d}\n",.{addFive(y)});
-    std.debug.print("Val fibonacci(y):{d}\n",.{fibonacci(y)});
+    std.debug.print("x after defer:{d}\n",.{x});
 }
