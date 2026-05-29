7a5d3549c07ec6bf471c0d8ddf96cec6644e0e3c
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-29 11:29:39 +0100

    play with vars and conts

--- main.zig
+++ main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
 pub fn main() void{
-  std.debug.print("Hello, {s}\n",.{"World"});
+    std.debug.print("Hello, {s}\n",.{"World"});
+
+    const a: i32 = 5;
+    const b:i32 = 5000;
+
+    std.debug.print("VAL: {d}\n",.{a+b});
 }
