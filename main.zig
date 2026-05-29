c4149a0754dc6a275880641a437fb7210ed43796
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-29 11:39:09 +0100

    arrays with explicit and non-explicit size

--- main.zig
+++ main.zig
@@ -2,9 +2,15 @@ const std = @import("std");
 
 pub fn main() void{
     std.debug.print("Hello, {s}\n",.{"World"});
-
+    
     const a: i32 = 5;
     const b:i32 = 5000;
 
     std.debug.print("VAL: {d}\n",.{a+b});
+
+    const c = [5]i32{1,2,3,4,5};
+    const d = [_]i32{};
+    
+    std.debug.print("LEN c: {d}\n",.{c.len});
+    std.debug.print("LEN d: {d}\n",.{d.len});
 }
