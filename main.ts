a8eaaabd203a23bfbef1641a49dc29bd952944ac
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-06-01 15:42:40 +0100

    represent the cursor x and y via type

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 import r, { InitWindow, SetTargetFPS, WindowShouldClose } from 'raylib';
 
+interface cursor_type{
+  x:number
+  y:number
+}
+
 interface model{
-  cursor:number
+  cursor:cursor_type
   char_buffer:string 
 }
 
 function render_window(){
   const screenWidth = 800;
   const screenHeight = 400;
+  const m:model = {cursor: {x:0,y:0}, char_buffer: ""};
 
   r. InitWindow(screenWidth,screenHeight, "Sumi");
   r.SetTargetFPS(60);
@@ -15,14 +21,13 @@ function render_window(){
   while(!WindowShouldClose()){
     r.BeginDrawing();
     r.ClearBackground(r.RAYWHITE);
-    r.DrawRectangle(0,0,10,20,r.GREEN);
+    r.DrawRectangle(m.cursor.x,m.cursor.y,10,20,r.GREEN);
     r.EndDrawing();
   }
   r.CloseWindow();
 }
 
 function main(){
-  const m:model = {cursor: 0, char_buffer: ""};
   render_window();
 }
 
