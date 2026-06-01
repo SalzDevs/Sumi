9ea167394df2689407db6f7ff394c59f3fc9e671
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-06-01 15:06:26 +0100

    create the model type

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import r, { InitWindow, SetTargetFPS, WindowShouldClose } from 'raylib';
 
-function main(){
+interface model{
+  cursor:number
+  char_buffer:string 
+}
+
+function render_window(){
   const screenWidth = 800;
   const screenHeight = 400;
 
- r. InitWindow(screenWidth,screenHeight, "Sumi");
-
+  r. InitWindow(screenWidth,screenHeight, "Sumi");
   r.SetTargetFPS(60);
 
   while(!WindowShouldClose()){
@@ -15,6 +19,11 @@ function main(){
     r.EndDrawing();
   }
   r.CloseWindow();
+}
+
+function main(){
+  const m:model = {cursor: 1, char_buffer: "hello my guy"};
+  console.log("model:",m);
 }
 
 main();
