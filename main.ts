ad6573c0e2ebde3f585023c049cc80a26cb7ef82
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-06-01 16:48:00 +0100

    refactor to the validate move funtion

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import r, { InitWindow, SetTargetFPS, WindowShouldClose } from 'raylib';
+import {Queue} from '@datastructures-js/queue';
 
 const screenWidth = 800;
 const screenHeight = 400;
@@ -16,29 +17,35 @@ interface model{
   char_buffer:string 
 }
 
+//TODO: Refactor this part (validate_next_position should only take the cursor)
 function validate_next_position(next_x:number, next_y:number,c:cursor_type) :boolean{
-  if (next_x<0 || next_x>screenWidth-c.width || next_y<0||next_y>screenHeight-c.height) return false;
-  console.log("x:  y:",next_x,next_y);
-  return true; 
+  return (next_x<0 || next_x>screenWidth-c.width || next_y<0||next_y>screenHeight-c.height);
 }
 
 function render_window(){
   const m:model = {cursor: {x:0,y:0,width:10,height:20}, char_buffer: ""};
+  const q = new Queue<string>();
 
   r. InitWindow(screenWidth,screenHeight, "Sumi");
   r.SetTargetFPS(60);
 
   while(!WindowShouldClose()){
+    //TODO: Refactor this part (validate_next_position should only take the cursor)
     if (r.IsKeyDown(r.KEY_RIGHT) && validate_next_position(m.cursor.x+1,m.cursor.y,m.cursor)) m.cursor.x+=1;
     if (r.IsKeyDown(r.KEY_DOWN) && validate_next_position(m.cursor.x,m.cursor.y+1,m.cursor)) m.cursor.y+=1;
     if (r.IsKeyDown(r.KEY_LEFT) && validate_next_position(m.cursor.x-1,m.cursor.y,m.cursor)) m.cursor.x-=1;
     if (r.IsKeyDown(r.KEY_UP) && validate_next_position(m.cursor.x,m.cursor.y-1,m.cursor)) m.cursor.y-=1;
+    const rune = r.GetKeyPressed();
+    if ((rune!=0) && !(r.IsKeyDown(r.KEY_RIGHT)) && !(r.IsKeyDown(r.KEY_DOWN)) && !(r.IsKeyDown(r.KEY_LEFT)) && !(r.IsKeyDown(r.KEY_UP))){
+       q.push(String.fromCharCode(rune)); 
+    }
 
     r.BeginDrawing();
     r.ClearBackground(r.RAYWHITE);
     r.DrawRectangle(m.cursor.x,m.cursor.y,m.cursor.width,m.cursor.height,r.GREEN);
     r.EndDrawing();
   }
+  console.log(q)
   r.CloseWindow();
 }
 
