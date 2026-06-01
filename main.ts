24b0ac8bd66ebe102e7d16606f22564e84cc0f1e
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-06-01 14:55:58 +0100

    raylib with ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-console.log("hello");
+import r, { InitWindow, SetTargetFPS, WindowShouldClose } from 'raylib';
+
+function main(){
+  const screenWidth = 800;
+  const screenHeight = 400;
+
+ r. InitWindow(screenWidth,screenHeight, "Sumi");
+
+  r.SetTargetFPS(60);
+
+  while(!WindowShouldClose()){
+    r.BeginDrawing();
+    r.ClearBackground(r.RAYWHITE);
+    r.DrawText("Congrats! You created your first window!", 190, 200, 20, r.LIGHTGRAY);
+    r.EndDrawing();
+  }
+  r.CloseWindow();
+}
+
+main();
